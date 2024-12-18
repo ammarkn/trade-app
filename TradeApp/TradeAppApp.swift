8ef7d1d272e5d7a21f90bf330d1182bd5a56006e
Ammar <ammarkhan5@yahoo.com>
2024-12-18 17:44:42 +0300

    Initial Commit

---
+++ TradeApp/TradeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeAppApp.swift
+//  TradeApp
+//
+//  Created by Ammar Khan on 2024-12-18.
+//
+
+import SwiftUI
+
+@main
+struct TradeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TradeAppTests/TradeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TradeAppTests.swift
+//  TradeAppTests
+//
+//  Created by Ammar Khan on 2024-12-18.
+//
+
+import Testing
+@testable import TradeApp
+
+struct TradeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
